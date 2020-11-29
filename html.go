dbabf3cbd63797032acf2d1c38909484f0de45b1
Sangeet Pathak <sangeetmeister@gmail.com>
2020-11-29 22:21:39 +0530

    Added working code with HTML to input username, simple password pair and display IP if it matches the one in YAML

---
+++ html.go
@@ -0,0 +1,12 @@
+package main
+
+var LoginHTML = `
+  <form action="/">
+  <br>
+  Username: <input type="text" name="username">
+  <br>
+  Password: <input type="password" name="password"><br>
+  Hash(Salt): <input type="text" name="hash"> <br>
+  <input type="submit" value="Submit">
+  </form> 
+  `
--- main.go
+++ main.go
@@ -6,9 +6,10 @@ import (
         "net/http"
 )
 
+var trivial_password string
 
 func main() {
-var trivial_password string
+//var trivial_password string
 
        viper.SetConfigName("ipss_config")         // name of config file (without extension)
         viper.AddConfigPath("$HOME/")             // path to config, call multiple times to add many search paths
@@ -24,10 +25,29 @@ var trivial_password string
 
 
         // Route => handler
-        e.GET("/", func(c echo.Context) error {
-                return c.HTML(http.StatusOK, "Hello, World!\n")
-        })
+//        e.GET("/", func(c echo.Context) error {
+//                return c.HTML(http.StatusOK, "Hello, World!\n")
+//        })
+
+	e.GET("/", RouteHandler)
 
         // Start server
         e.Logger.Fatal(e.Start(":1323"))
+}
+
+func RouteHandler(c echo.Context) error {
+	uname := c.FormValue("username")
+	pass := c.FormValue("password")
+	hash := c.FormValue("hash")
+	if uname != "" {
+		if pass == trivial_password {
+			ip := c.RealIP()
+			return c.HTML(http.StatusOK, LoginHTML + ip + hash)
+		} else {
+			fmt.Println("Simple password did not match")
+		}
+	} else {
+		fmt.Println("Username cannot be blank")
+	}
+	return c.HTML(http.StatusOK, LoginHTML)
 }
